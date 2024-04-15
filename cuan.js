41c68fbf8e519dac1fd4de6ba817c9bcdbacdea5
Rduardian <167046865+Rduardian@users.noreply.github.com>
2024-04-15 13:05:40 +0700

    Add files via upload

---
+++ cuan.js
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<script type='text/javascript' src='//pl23068672.profitablegatecpm.com/36/aa/2b/36aa2ba179c4d30d7087efc8e1b3dad8.js'></script>
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="css/index.css">
+  <script src="js/index.js"></script>
+  <title>Index.html</title>
+ 
+</head>
+<body>
+</body>
